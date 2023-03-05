e3c968d3ddfae2884c2f09b54bbd339a4dbbf9e8
Md Rifad Ahmed <rifad.mbstu@gmail.com>
2023-03-05 13:28:15 +0100

    finding the factorial of a number

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int i, fact=1;
+        for (i=4; i>=1; i--){
+            fact = fact * i;}
+        {
+            System.out.println("The factorial of the number is:" +fact);
+        }
+    }
+}
